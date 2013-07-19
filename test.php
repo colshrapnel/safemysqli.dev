3def70ae2a1ba94d588575e9a68fc1028a181a35
colshrapnel <col.shrapnel@gmail.com>
2013-07-19 16:59:58 +0400

    Create test.php

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+
+include 'safemysqli.class.php';
+$db = new SafeMySQLi(['user' =>'test', 'pass' => 'aaa',"emode"=>"exception"]);
+
+$sql = "SELECT :one FROM n:two WHERE f = i:tre OR f = i:tre OR f = :four\n";
+$args['one'] = 'hello';
+$args['two'] = 'table';
+$args['tre'] = 5;
+$args[':four'] = 666;
+echo $db->prepare($sql, $args);
+
+$sql = "SELECT ?s FROM ?n WHERE f = ?i OR f = ?\n";
+$args = array('hello', 'table', 5,666);
+echo $db->prepare($sql, $args);
+
+echo $db->prepare("SELECT c FROM t \n");
